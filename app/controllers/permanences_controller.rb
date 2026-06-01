b0585ab7497c572141a87becb70b54e56901aea1
Solene Bloret <solene.bloret@hotmail.fr>
2026-06-01 17:32:02 +0200

    création des controllers pour chaque model

---
+++ app/controllers/permanences_controller.rb
@@ -0,0 +1,2 @@
+class PermanencesController < ApplicationController
+end
---
+++ test/controllers/permanences_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PermanencesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
