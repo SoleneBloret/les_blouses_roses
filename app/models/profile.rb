0b7bbe8cd9fc273ec6ab3ef4313331605f00423f
Solene Bloret <solene.bloret@hotmail.fr>
2026-06-02 16:35:52 +0200

    ajout de validations aux profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :role, presence: true
+
+  validates :phone_number, presence: true
+  validates :phone_number, uniqueness: true
+  validates :phone_number, format: { with: /^(?:0|\+33 ?|0?0?33 ?|)([1-9] ?(?:[0-9] ?){8})$/, message: "Numéros français uniquement" }
 end
