fa44e62170052cddb1873668aea354575121acde
Solene Bloret <solene.bloret@hotmail.fr>
2026-06-02 15:53:11 +0200

    paramétrage des vues new, edit et show du profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,30 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: %i[show edit update]
-
   def new
     @profile = Profile.new
+    @profile.user = current_user
   end
 
   def create
-    @profile = Profile.new(profile.params)
+    @profile = Profile.new(profile_params)
     @profile.user = current_user
     if @profile.save
-      redirect_to profile_path(@profile), notice: "Votre profile a bien été créé"
+      redirect_to profile_path(@profile), notice: "Votre profil a bien été créé"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
   def edit
+    @profile = Profile.find(params[:id])
   end
 
   def update
-    if @profile.update(profile.params)
+    @profile = Profile.find(params[:id])
+    if @profile.update(profile_params)
       redirect_to profile_path(@profile), notice: "Votre profil a été mis à jour"
     else
       render :edit, status: :unprocessable_entity
@@ -32,11 +34,7 @@ class ProfilesController < ApplicationController
   # ------------ METHODES PRIVEES ------------
   private
 
-  def profile.params
+  def profile_params
     params.require(:profile).permit(:first_name, :last_name, :phone_number, :address, :role)
-  end
-
-  def set_profile
-    @profile = Profile.find(params[:id])
   end
 end
