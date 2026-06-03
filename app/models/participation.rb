663498bde51ea4d60aaf576ef407b83283ed5faf
Solene Bloret <solene.bloret@hotmail.fr>
2026-06-03 15:32:23 +0200

    amélioration du front sur la vue index des participations

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,14 @@
 class Participation < ApplicationRecord
   belongs_to :permanence
   belongs_to :user
+
+  FRENCH_WEEKDAYS = {
+    "Lundi" => 1, "Mardi" => 2, "Mercredi" => 3,
+    "Jeudi" => 4, "Vendredi" => 5, "Samedi" => 6, "Dimanche" => 7
+  }.freeze
+
+  def date
+    day_number = FRENCH_WEEKDAYS[permanence.week_day]
+    Date.commercial(permanence.year, week_number, day_number)
+  end
 end
